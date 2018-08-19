9932681368b68b06f993cae770354b601149cb79
Keith <keith-la@S280023.csusm.edu>
2018-08-19 09:10:38 -0700

    Added Linux and Windows compatibility

--- usv_sort.py
+++ usv_sort.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-"""Author: Aurelio Arnago"""
+"""Author: Aurelio Arango"""
 #from os import listdir
 #from os.path import isfile, join
 
@@ -10,10 +10,8 @@ import random
 import matplotlib.image as mpimg
 from PIL import Image
 import os
+import platform
 import numpy as np
-
-
-
 
 """READ PATH
 PARAM list of arguments from input
@@ -34,6 +32,8 @@ def read_path(argv):
             dir_path = arg
         elif opt in ("-p","--ppercent" ):
             percent = int(arg)/100.0
+        else:
+            print("usv_sort.py -i <usv_dir_path> - p <validation_percent>")
 
     print('Dir Path is ', dir_path)
     print("Validation Percent ", percent)
@@ -43,44 +43,37 @@ def split_data(path, percent):
     f = []
     for (dirpath, dirnames, filenames) in walk(path):
         f.extend(filenames)
-    #break
-    print(filenames)
+
     # Get dir name
     last_position =path.rfind("\\")
     dir_name = path[last_position+1:]
     local_path = path[:last_position]
-    #print(dir_name)
-    #print(local_path)
 
-    # Create Paths
-    val_path = local_path + "\\Validation\\" + dir_name
-    test_path = local_path + "\\Training\\" +dir_name
+    val_path = ''
+    test_path = ''
+    if platform.system() == "Windows":
+        # Create Paths
+        val_path = local_path + "\\Validation\\" + dir_name
+        test_path = local_path + "\\Training\\" + dir_name
+    else:
+        val_path = local_path + "/Validation/" + dir_name
+        test_path = local_path + "/Training/" + dir_name
     # Create Dir
     pathlib.Path(val_path).mkdir(parents=True, exist_ok=True)
     pathlib.Path(test_path).mkdir(parents=True, exist_ok=True)
-    #make sure we are in the correct directory
-
 
     # Iterate through file list
     for index in range (len(filenames)):
-        #print(filenames[index])
-        # read image
-        #img = mpimg.imread(filenames[index])
+        # make sure we are in the correct directory
         os.chdir(path)
+        # read image
         img = Image.open(filenames[index])
-
-        #im = Image.open(filenames[index])
-        #im.show()
+        # Randomly decide where to store image
         if random.uniform(0, 1) < percent:
-            print("less than")
             os.chdir(val_path)
-            #mpimg.imsave(filenames[index])
             img.save(filenames[index])
         else:
-            print("grater")
             os.chdir(test_path)
-            #mpimg.imsave(filenames[index])
-            #mpimg.
             img.save(filenames[index])
 
 
